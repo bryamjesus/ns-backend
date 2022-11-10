9ecea4229b0d4993ad5fe86b1b061d8649fcc63e
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-10 18:41:04 -0500

    Add Auth.js

---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const { verify } = require("jsonwebtoken");
+const { TOKEN_KEY } = require("../config");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log(token);
+  if (token === null) {
+    return res.status(401).send("Token requerido");
+  }
+  verify(token, TOKEN_KEY, (err, result) => {
+    if (err) {
+      return res.status(403).send("Token inválido");
+    }
+    console.log(result);
+    req.datos = result;
+    next();
+  });
+};
+
+module.exports = verifyToken;
